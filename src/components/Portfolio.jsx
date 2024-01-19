c0b9d0256072e9821d4d5e8194a6c5d72aac5dde
mayankmj <mayankjohari877@gmail.com>
2024-01-20 01:51:05 +0530

    onestop-link update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -22,7 +22,7 @@ const Portfolio = () => {
       src: OneStop,
       details:'A real-time web application developed to provide users with a platform for posting events in a categorized manner and engaging with other users through the comment section.',
       header:'OneStop',
-      demo:'https://onestop1.netlify.app/',
+      demo:'https://onestopsol.netlify.app',
       code:'https://github.com/mayankmj/onestop',
     },
     {
